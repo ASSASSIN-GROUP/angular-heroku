2c30a8ba443d5e86a4e48b300d839875abc762ff
ninjadev1030 <dalianwang1030@yahoo.com>
2018-01-09 04:11:31 +0100

    add date

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  'news': {
+    'id': 12345,
+    'author': 'John Doe',
+    'tags': 'webdesign,  mobile, software development',
+    'imageurl': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2rjhHwUcSX19nHJ4g0TLGVZOBVsdl694baHym8_yybDKiacjR2w (10 kB)',
+    'content': 'Software Mobile',
+    'category': 'mobile development'
+  }
+  now = new Date()
+  year = this.now.getFullYear()
+  month = this.now.getMonth() + 1
+  day = this.now.getDate()
+  date = this.day + '/' + this.month + '/' + this.year
+  
   constructor() { }
 
   ngOnInit() {
+    
   }
 
 }
